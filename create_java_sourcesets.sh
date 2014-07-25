9e08c2c127c2bec7414da6c3a39ec49f0ffa275e
Tomas Longo <longo.tomas@googlemail.com>
2014-07-25 21:46:34 +0200

    Created script to create java source sets

---
+++ create_java_sourcesets.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Creates the standard sourcesets of a java application in the passed location
+# Creates:
+#  src
+#    main
+#      java
+#      resources
+#    test
+#      java
+#      resources
+
+location="."
+if [ $# -eq 1 ] 
+  then
+    # param was supplied
+    location=$1
+  elif [ $# -gt 1 ]
+    then
+      echo 'too much params provided...'
+      echo 'usage: create_java_sourcesets.sh [location]'
+      exit -1
+fi
+
+mkdir -p $location/src/main/java
+mkdir -p $location/src/main/resources
+
+mkdir -p $location/src/test/java
+mkdir -p $location/src/test/resources
